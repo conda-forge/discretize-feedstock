17e98b60b10dd121bd00e660bc4b41d1e708a827
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-10-28 12:39:00 -0600

    updates for build scripts

---
+++ recipe/build.sh
@@ -0,0 +1,8 @@
+mkdir builddir
+
+# need to run meson first for cross-compilation case
+${PYTHON} $(which meson) setup ${MESON_ARGS} \
+    builddir || (cat builddir/meson-logs/meson-log.txt && exit 1)
+
+${PYTHON} -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
+${PYTHON} -m pip install dist/discretize*.whl
