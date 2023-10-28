684bb377119ae5c276521432c7a1a7ad66874767
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-10-28 14:14:41 -0600

    set readonly variable at first call to meson

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@ set -ex
 
 mkdir builddir
 
-meson ${MESON_ARGS} builddir/
+meson ${MESON_ARGS} --vsenv builddir/
 
 # ${PYTHON} -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
 # ${PYTHON} -m pip install dist/discretize*.whl
