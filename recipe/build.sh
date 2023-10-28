6006959b80d88ac2a2b94d7e80783b800fc1061f
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-10-28 13:17:16 -0600

    specify builddir for meson

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,4 +7,4 @@ meson ${MESON_ARGS} builddir/
 # ${PYTHON} -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
 # ${PYTHON} -m pip install dist/discretize*.whl
 
-${PYTHON} -m pip install --no-deps -vv --no-build-isolation .
+${PYTHON} -m pip install --no-deps -vv --no-build-isolation --config-settings=builddir="builddir" .
